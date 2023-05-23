44cae928bfb885e001dc93f665e4f279b15052c3
Kozlowski-Bartosz <kozlowski.bartosz@protonmail.com>
2023-05-23 18:56:27 +0200

    Translated code to English

--- etap1.py
+++ etap1.py
@@ -4,10 +4,10 @@ from matplotlib.widgets import CheckButtons, RadioButtons
 
 # Parametry sygnałów
 A1 = 1
-f1 = 1
+f1 = 4.35
 fi1 = 0
 A2 = 0.5
-f2 = 2
+f2 = 8.7
 fi2 = np.pi/4
 
 # Inicjalizacja wykresów
@@ -15,16 +15,16 @@ fig, ax = plt.subplots()
 fig2, axs2 = plt.subplots()
 
 # Tworzenie wektora czasu
-czas_trwania = 20
-okres_probkowania = 0.01
-czas = np.arange(0, czas_trwania, okres_probkowania)
-window = np.ones(len(czas))
+signal_time = 20
+sampling_frequency = 0.01
+time = np.arange(0, signal_time, sampling_frequency)
+window = np.ones(len(time))
 
 # Inicjalizacja linii wykresów
-linia1, = ax.plot(czas, np.zeros_like(czas), label='Sygnał 1')
-linia2, = ax.plot(czas, np.zeros_like(czas), label='Sygnał 2')
-linia3, = ax.plot(czas, np.zeros_like(czas), label='Sygnał Złożony')
-linia4, = axs2.plot([], [])
+line_1, = ax.plot(time, np.zeros_like(time), label='Sygnał 1')
+line_2, = ax.plot(time, np.zeros_like(time), label='Sygnał 2')
+line_3, = ax.plot(time, np.zeros_like(time), label='Sygnał Złożony')
+line_4, = axs2.plot([], [])
 
 # Konfiguracja wykresów
 ax.set_title('Sygnał')
@@ -35,32 +35,34 @@ axs2.set_title('Transformata Fouriera')
 axs2.set_xlabel('Częstotliwość [Hz]')
 axs2.set_ylabel('Amplituda')
 
-polowa_indeksow = int(np.ceil(len(czas) / 2))
+index_half = int(np.ceil(len(time) / 2))
 
 # Funkcja aktualizująca wykresy
 def update():
-    global A1, f1, fi1, A2, f2, fi2, czas_trwania, okres_probkowania, czas, window
+    global A1, f1, fi1, A2, f2, fi2, signal_time, sampling_frequency, time, window
     
-    czas = np.arange(0, czas_trwania, okres_probkowania)
+    
+    
+    time = np.arange(0, signal_time, sampling_frequency)
     update_window(buttons_window_obj.value_selected)
 
     # Sygnały sinusoidalne
-    sygnal1 = A1 * np.sin(2 * np.pi * f1 * czas + fi1) * window
-    sygnal2 = A2 * np.sin(2 * np.pi * f2 * czas + fi2) * window
-    sygnal = sygnal1 + sygnal2
+    signal1 = A1 * np.sin(2 * np.pi * f1 * time + fi1) * window
+    signal2 = A2 * np.sin(2 * np.pi * f2 * time + fi2) * window
+    signal_combined = signal1 + signal2
     
-    linia1.set_data(czas, sygnal1)
-    linia2.set_data(czas, sygnal2)
-    linia3.set_data(czas, sygnal)
-    ax.set_xlim([czas[0], czas[-1]])
+    line_1.set_data(time, signal1)
+    line_2.set_data(time, signal2)
+    line_3.set_data(time, signal_combined)
+    ax.set_xlim([time[0], time[-1]])
     ax.set_ylim([-5.0, 5.0])
 
-    widmo = np.abs(np.fft.fft(sygnal)[:polowa_indeksow])
-    maksimum_widmo = np.max(widmo)
+    freq_spectrum = np.abs(np.fft.fft(signal_combined)[:index_half])
+    spectrum_max = np.max(freq_spectrum)
 
-    linia4.set_data(np.fft.fftfreq(len(czas), okres_probkowania)[:polowa_indeksow], widmo)
+    line_4.set_data(np.fft.fftfreq(len(time), sampling_frequency)[:index_half], freq_spectrum)
     axs2.set_xlim(0, 10)
-    axs2.set_ylim([np.min(widmo), maksimum_widmo if maksimum_widmo > 500 else 500])
+    axs2.set_ylim([np.min(freq_spectrum), spectrum_max if spectrum_max > 500 else 500])
 
     fig.canvas.draw_idle()
     fig2.canvas.draw_idle()
@@ -70,8 +72,8 @@ plt.subplots_adjust(bottom=0.65)
 axcolor = 'lightgoldenrodyellow'
 buttons_window = plt.axes([0.65, 0.5, 0.3, 0.08])
 buttons_signal = plt.axes([0.65, 0.4, 0.3, 0.08])
-slider_czas_trwania = plt.axes([0.15, 0.45, 0.4, 0.03], facecolor=axcolor)
-slider_okres_probkowania = plt.axes([0.15, 0.4, 0.4, 0.03], facecolor=axcolor)
+slider_signal_time = plt.axes([0.15, 0.45, 0.4, 0.03], facecolor=axcolor)
+slider_sampling_frequency = plt.axes([0.15, 0.4, 0.4, 0.03], facecolor=axcolor)
 slider_A1 = plt.axes([0.15, 0.35, 0.7, 0.03], facecolor=axcolor)
 slider_f1 = plt.axes([0.15, 0.3, 0.7, 0.03], facecolor=axcolor)
 slider_fi1 = plt.axes([0.15, 0.25, 0.7, 0.03], facecolor=axcolor)
@@ -81,8 +83,8 @@ slider_fi2 = plt.axes([0.15, 0.1, 0.7, 0.03], facecolor=axcolor)
 
 buttons_window_obj = RadioButtons(buttons_window, ('Brak', 'Hamminga', 'Barletta', 'Blackmana'), active=0)
 buttons_signal_obj = CheckButtons(buttons_signal, ('Sygnał 1', 'Sygnał 2', 'Sygnał Złożony'), (True, True, True))
-slider_czas_trwania_obj = plt.Slider(slider_czas_trwania, 'Czas trwania', 1, 30, valinit=czas_trwania, valstep=1)
-slider_okres_probkowania_obj = plt.Slider(slider_okres_probkowania, 'Okres próbkowania', 0.01, 1.0, valinit=okres_probkowania, valstep=0.01)
+slider_signal_time_obj = plt.Slider(slider_signal_time, 'Czas trwania', 1, 30, valinit=signal_time, valstep=1)
+slider_sampling_frequency_obj = plt.Slider(slider_sampling_frequency, 'Okres próbkowania', 0.01, 1.0, valinit=sampling_frequency, valstep=0.01)
 slider_A1_obj = plt.Slider(slider_A1, 'A1', 0.01, 3.0, valinit=A1, valstep=0.01)
 slider_f1_obj = plt.Slider(slider_f1, 'f1', 0.001, 10.0, valinit=f1, valstep=0.001)
 slider_fi1_obj = plt.Slider(slider_fi1, 'fi1', 0.0, 2*np.pi, valinit=fi1, valstep=0.01)
@@ -91,7 +93,7 @@ slider_f2_obj = plt.Slider(slider_f2, 'f2', 0.001, 10.0, valinit=f2, valstep=0.0
 slider_fi2_obj = plt.Slider(slider_fi2, 'fi2', 0.0, 2*np.pi, valinit=fi2, valstep=0.01)
 
 def update_params(val):
-    global A1, f1, fi1, A2, f2, fi2, czas_trwania, okres_probkowania
+    global A1, f1, fi1, A2, f2, fi2, signal_time, sampling_frequency
 
     A1 = slider_A1_obj.val
     f1 = slider_f1_obj.val
@@ -99,22 +101,22 @@ def update_params(val):
     A2 = slider_A2_obj.val
     f2 = slider_f2_obj.val
     fi2 = slider_fi2_obj.val
-    czas_trwania = slider_czas_trwania_obj.val
-    okres_probkowania = slider_okres_probkowania_obj.val
+    signal_time = slider_signal_time_obj.val
+    sampling_frequency = slider_sampling_frequency_obj.val
 
     update()
     
 def update_window(label = ""):
-    global czas, window
+    global time, window
 
     if label == 'Hamminga':
-        window = np.hamming(len(czas))
+        window = np.hamming(len(time))
     elif label == 'Barletta':
-        window = np.bartlett(len(czas))
+        window = np.bartlett(len(time))
     elif label == 'Blackmana':
-        window = np.blackman(len(czas))
+        window = np.blackman(len(time))
     else:
-        window = np.ones(len(czas))
+        window = np.ones(len(time))
         
     return window
 
@@ -125,17 +127,17 @@ slider_fi1_obj.on_changed(update_params)
 slider_A2_obj.on_changed(update_params)
 slider_f2_obj.on_changed(update_params)
 slider_fi2_obj.on_changed(update_params)
-slider_czas_trwania_obj.on_changed(update_params)
-slider_okres_probkowania_obj.on_changed(update_params)
+slider_signal_time_obj.on_changed(update_params)
+slider_sampling_frequency_obj.on_changed(update_params)
 
 
 def toggle_visibility(label):
     if label == 'Sygnał 1':
-        linia1.set_visible(not linia1.get_visible())
+        line_1.set_visible(not line_1.get_visible())
     elif label == 'Sygnał 2':
-        linia2.set_visible(not linia2.get_visible())
+        line_2.set_visible(not line_2.get_visible())
     elif label == 'Sygnał Złożony':
-        linia3.set_visible(not linia3.get_visible())
+        line_3.set_visible(not line_3.get_visible())
 
     fig.canvas.draw_idle()
 
--- etap2.py
+++ etap2.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Parametry sygnałów
-amplituda = 1
 A = 1
 K = 1
 t1 = 1
@@ -16,13 +15,13 @@ fig, ax = plt.subplots()
 fig2, axs2 = plt.subplots()
 
 # Tworzenie wektora czasu
-czas_trwania = 20
-okres_probkowania = 0.01
-czas = np.arange(0, czas_trwania, okres_probkowania)
+signal_time = 20
+sampling_frequency = 0.01
+time = np.arange(0, signal_time, sampling_frequency)
 
 # Inicjalizacja linii wykresów
-linia1, = ax.plot(czas, np.zeros_like(czas))
-linia2, = axs2.plot([], [])
+line_1, = ax.plot(time, np.zeros_like(time))
+line_2, = axs2.plot([], [])
 
 # Konfiguracja wykresów
 ax.set_title('Sygnał')
@@ -33,28 +32,28 @@ axs2.set_title('Transformata Fouriera')
 axs2.set_xlabel('Częstotliwość [Hz]')
 axs2.set_ylabel('Amplituda')
 
-polowa_indeksow = int(np.ceil(len(czas) / 2))
+index_half = int(np.ceil(len(time) / 2))
 
 # Funkcja aktualizująca wykresy
 def update():
-    global A, K, t1, t2, n, f, fi, czas_trwania, okres_probkowania, czas
+    global A, K, t1, t2, n, f, fi, signal_time, sampling_frequency, time
     
-    czas = np.arange(0, czas_trwania, okres_probkowania)
+    time = np.arange(0, signal_time, sampling_frequency)
 
-    sygnal = A * K * ((czas/t1)**n)/(1 + (czas/t1)**n) * np.exp(-czas/t2) * np.cos(2*np.pi*f*czas + fi)
+    sygnal = A * K * ((time/t1)**n)/(1 + (time/t1)**n) * np.exp(-time/t2) * np.cos(2*np.pi*f*time + fi)
 
 
 
-    linia1.set_data(czas, sygnal)
-    ax.set_xlim([czas[0], czas[-1]])
-    ax.set_ylim([-5.0, 5.0])
+    line_1.set_data(time, sygnal)
+    ax.set_xlim([time[0], time[-1]])
+    ax.set_ylim([-5.0, 5.0] if np.max(sygnal) < 5.0 else [np.min(sygnal), np.max(sygnal)])
 
-    widmo = np.abs(np.fft.fft(sygnal)[:polowa_indeksow])
-    maksimum_widmo = np.max(widmo)
+    freq_spectrum = np.abs(np.fft.fft(sygnal)[:index_half])
+    spectrum_max = np.max(freq_spectrum)
 
-    linia2.set_data(np.fft.fftfreq(len(czas), okres_probkowania)[:polowa_indeksow], widmo)
+    line_2.set_data(np.fft.fftfreq(len(time), sampling_frequency)[:index_half], freq_spectrum)
     axs2.set_xlim(0, 10)
-    axs2.set_ylim([np.min(widmo), maksimum_widmo if maksimum_widmo > 500 else 500])
+    axs2.set_ylim([np.min(freq_spectrum), spectrum_max if spectrum_max > 500 else 500])
 
     fig.canvas.draw_idle()
     fig2.canvas.draw_idle()
@@ -62,8 +61,8 @@ def update():
 # Konfiguracja narzędzi interaktywnych
 plt.subplots_adjust(bottom=0.5)
 axcolor = 'lightgoldenrodyellow'
-slider_czas_trwania = plt.axes([0.15, 0.4, 0.7, 0.03], facecolor=axcolor)
-slider_okres_probkowania = plt.axes([0.15, 0.35, 0.7, 0.03], facecolor=axcolor)
+slider_signal_time = plt.axes([0.15, 0.4, 0.7, 0.03], facecolor=axcolor)
+slider_sampling_frequency = plt.axes([0.15, 0.35, 0.7, 0.03], facecolor=axcolor)
 slider_A = plt.axes([0.15, 0.3, 0.7, 0.03], facecolor=axcolor)
 slider_K = plt.axes([0.15, 0.25, 0.7, 0.03], facecolor=axcolor)
 slider_t1 = plt.axes([0.15, 0.2, 0.7, 0.03], facecolor=axcolor)
@@ -72,18 +71,18 @@ slider_n = plt.axes([0.15, 0.1, 0.7, 0.03], facecolor=axcolor)
 slider_f = plt.axes([0.15, 0.05, 0.7, 0.03], facecolor=axcolor)
 slider_fi = plt.axes([0.15, 0.0, 0.7, 0.03], facecolor=axcolor)
 
-slider_czas_trwania_obj = plt.Slider(slider_czas_trwania, 'Czas trwania', 1, 30, valinit=czas_trwania, valstep=1)
-slider_okres_probkowania_obj = plt.Slider(slider_okres_probkowania, 'Okres próbkowania', 0.01, 1.0, valinit=okres_probkowania, valstep=0.01)
+slider_signal_time_obj = plt.Slider(slider_signal_time, 'Czas trwania', 1, 30, valinit=signal_time, valstep=1)
+slider_sampling_frequency_obj = plt.Slider(slider_sampling_frequency, 'Okres próbkowania', 0.01, 1.0, valinit=sampling_frequency, valstep=0.01)
 slider_A_obj = plt.Slider(slider_A, 'A', 0.01, 10.0, valinit=A, valstep=0.01)
 slider_K_obj = plt.Slider(slider_K, 'K', 0.01, 10.0, valinit=K, valstep=0.01)
-slider_t1_obj = plt.Slider(slider_t1, 't1', 0.001, czas_trwania, valinit=t1, valstep=0.001)
-slider_t2_obj = plt.Slider(slider_t2, 't2', 0.001, czas_trwania, valinit=t2, valstep=0.001)
+slider_t1_obj = plt.Slider(slider_t1, 't1', 0.001, signal_time, valinit=t1, valstep=0.001)
+slider_t2_obj = plt.Slider(slider_t2, 't2', 0.001, signal_time, valinit=t2, valstep=0.001)
 slider_n_obj = plt.Slider(slider_n, 'n', 0.01, 10.0, valinit=n, valstep=0.01)
 slider_f_obj = plt.Slider(slider_f, 'f', 0.001, 10.0, valinit=f, valstep=0.001)
 slider_fi_obj = plt.Slider(slider_fi, 'fi', 0.0, 2*np.pi, valinit=fi, valstep=0.01)
 
 def update_params(val):
-    global A, K, t1, t2, n, f, fi, czas_trwania, okres_probkowania
+    global A, K, t1, t2, n, f, fi, signal_time, sampling_frequency
 
     A = slider_A_obj.val
     K = slider_K_obj.val
@@ -92,8 +91,8 @@ def update_params(val):
     n = slider_n_obj.val
     f = slider_f_obj.val
     fi = slider_fi_obj.val
-    czas_trwania = slider_czas_trwania_obj.val
-    okres_probkowania = slider_okres_probkowania_obj.val
+    signal_time = slider_signal_time_obj.val
+    sampling_frequency = slider_sampling_frequency_obj.val
 
     update()
 
@@ -104,8 +103,8 @@ slider_t2_obj.on_changed(update_params)
 slider_n_obj.on_changed(update_params)
 slider_f_obj.on_changed(update_params)
 slider_fi_obj.on_changed(update_params)
-slider_czas_trwania_obj.on_changed(update_params)
-slider_okres_probkowania_obj.on_changed(update_params)
+slider_signal_time_obj.on_changed(update_params)
+slider_sampling_frequency_obj.on_changed(update_params)
 
 # Wywołanie funkcji update dla inicjalizacji wykresów
 update()
